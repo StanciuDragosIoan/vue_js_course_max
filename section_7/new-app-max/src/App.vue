39a5ee553dfc6ea830efd914c4b841fa109f7563
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-02-16 14:29:15 +0200

    test

---
+++ section_7/new-app-max/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lawrence",
+          phone: "555-5555-5555",
+          email: "manuel@localhost.com",
+        },
+
+        {
+          id: "julie",
+          name: "Julie Lawrence",
+          phone: "555-5555-5555",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ section_7/vue-first-app/notes.js
@@ -0,0 +1,11 @@
+/*
+.vue file
+
+it has a <template></template> element
+
+a <script>
+
+and a style tag
+
+
+*/

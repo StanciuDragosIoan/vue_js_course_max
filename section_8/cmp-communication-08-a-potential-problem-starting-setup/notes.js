f3faf87170c8ec16d1ba9eb3569cdb27edd435fb
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-03-01 14:25:32 +0200

    test

---
+++ section_8/cmp-communication-08-a-potential-problem-starting-setup/notes.js
@@ -0,0 +1,10 @@
+/*
+
+provide/inject can be used to replace props + custom events communication
+
+props and custom events are the default
+
+use provide/inject when faced with a context in which you have a 
+'pass through' component that only forwards the event call
+
+*/

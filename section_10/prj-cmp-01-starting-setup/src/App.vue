4ec07de61976a996f24941286e453f1efb0295c0
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-03-08 15:10:39 +0200

    test

---
+++ section_10/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official vuejs documentation',
+          link: 'https://vuejs.org'
+        },
+
+        {
+          id: 'google',
+          title: 'Google is your friend',
+          description: 'It is very important to learn how to google stuff',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  }
+};
+</script>

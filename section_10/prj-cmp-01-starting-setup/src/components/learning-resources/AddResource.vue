1d3cbfa5301c54c69e7e4e4d5dcc2daa85284d49
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-03-11 11:45:47 +0200

    test

--- section_10/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
+++ section_10/prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -1,3 +1,100 @@
 <template>
-  <h2>Add Resource</h2>
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid input"
+    @close="confirmError"
+  >
+    <template #default>
+      <p>Unfortunately at least one input value is invalid.</p>
+      <p>Please check all inputs.</p>
+    </template>
+
+    <template #actions>
+      <button @click="confirmError">Ok</button>
+    </template>
+  </base-dialog>
+  <base-card>
+    <form @submit.prevent="submitData">
+      <div class="form-control">
+        <label>Title</label>
+        <input id="title" name="title" type="text" ref="titleInput" />
+      </div>
+      <div class="form-control">
+        <label>Description</label>
+        <textarea
+          name="description"
+          id="description"
+          rows="3"
+          ref="descInput"
+        ></textarea>
+      </div>
+      <div class="form-control">
+        <label>Link</label>
+        <input id="link" name="link" type="url" ref="linkInput" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
+<script>
+import BaseDialog from '../UI/BaseDialog.vue';
+
+export default {
+  components: { BaseDialog },
+  inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false
+    };
+  },
+  methods: {
+    submitData() {
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredDescription = this.$refs.descInput.value;
+      const enteredLink = this.$refs.linkInput.value;
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredLink.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+      this.addResource(enteredTitle, enteredDescription, enteredLink);
+    },
+
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
+  }
+};
+</script>
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
--- section_10/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
+++ section_10/prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -11,7 +11,9 @@
       >Add Resource</base-button
     >
   </base-card>
-  <component :is="selectedTab"></component>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
 </template>
 
 <script>
@@ -46,7 +48,8 @@ export default {
 
   provide() {
     return {
-      resources: this.storedResources
+      resources: this.storedResources,
+      addResource: this.addResource
     };
   },
 
@@ -63,6 +66,18 @@ export default {
   methods: {
     setSelectedTab(tab) {
       this.selectedTab = tab;
+    },
+
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title,
+        description,
+        link: url
+      };
+
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
     }
   }
 };

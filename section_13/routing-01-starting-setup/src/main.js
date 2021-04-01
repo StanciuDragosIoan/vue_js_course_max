3bd1c16735ad285aafae7db732ced638cddd8b3a
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-04-01 11:39:29 +0300

    test

--- section_13/routing-01-starting-setup/src/main.js
+++ section_13/routing-01-starting-setup/src/main.js
@@ -5,23 +5,43 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      name: 'teams',
+      path: '/',
+      redirect: 'teams'
+    },
+    {
       path: '/teams',
-      component: TeamsList //our-domain.com/teams =>
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter
+      }, //our-domain.com/teams =>,
+      // alias: '/'
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true
+        }
+      ]
     },
 
     {
       path: '/users',
-      component: UsersList //our-domain.com/teams =>
+      components: { default: UsersList, footer: UsersFooter } //our-domain.com/teams =>
     },
+
     {
-      path: '/teams/:teamId',
-      component: TeamMembers,
-      props: true
+      path: '/:notFound(.*)',
+      component: NotFound
     }
   ]
 });

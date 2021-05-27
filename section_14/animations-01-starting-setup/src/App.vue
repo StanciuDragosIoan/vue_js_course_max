8930ea71bdb64509bea381b3f3501fba3f2d088d
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-05-27 14:39:10 +0300

    test

--- section_14/animations-01-starting-setup/src/App.vue
+++ section_14/animations-01-starting-setup/src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="container">
-    <div class="block" :class="{animate: animatedBlock}"></div>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
+  </div>
+  <div class="container">
+    <transition>
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -10,14 +16,15 @@
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
-</template>  
+</template>
 
 <script>
 export default {
   data() {
-    return { 
+    return {
       animatedBlock: false,
-      dialogIsVisible: false 
+      dialogIsVisible: false,
+      paraIsVisible: false
     };
   },
   methods: {
@@ -30,7 +37,10 @@ export default {
     animateBlock() {
       this.animatedBlock = true;
     },
-  },
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
+    }
+  }
 };
 </script>
 
@@ -81,6 +91,33 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 0.5s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-active {
+  transition: all 0.5s ease-in;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
 @keyframes slide-fade {
   0% {
     transform: translateX(0) scale(1);

8cdfc3635b441a51ff1afa59a33560b9becf1d54
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-06-07 16:25:01 +0300

    test

--- section_14/animations-01-starting-setup/src/components/UsersList.vue
+++ section_14/animations-01-starting-setup/src/components/UsersList.vue
@@ -1,13 +1,13 @@
 <template>
-  <ul>
+  <transition-group tag="ul" name="user-list">
     <li v-for="user in users" :key="user" @click="removeUser(user)">
       {{ user }}
-    </li>
-    <div>
-      <input type="text" ref="userNameInput" />
-      <button @click="addUser">Add User</button>
-    </div>
-  </ul>
+    </li></transition-group
+  >
+  <div>
+    <input type="text" ref="userNameInput" />
+    <button @click="addUser">Add User</button>
+  </div>
 </template>
 <script>
 export default {
@@ -38,5 +38,34 @@ li {
   border: 1px solid #ccc;
   padding: 1rem;
   text-align: center;
+}
+
+.user-list-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.user-list-enter-active {
+  transition: all 1s ease-out;
+}
+.user-list-enter-to {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.user-list-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+.user-list-leave-active {
+  transition: all 1s ease-in;
+  position: absolute;
+}
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
+.user-list-move {
+  transition: transform 0.8s ease;
 }
 </style>
